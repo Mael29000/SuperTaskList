6010da63a13e1580b59f7044a779b87a315677d5
Mael29000 <maelsuard@orange.fr>
2023-05-19 17:41:01 +0200

    update icons to include svg + change color

--- src/screens/Tasks/TaskTile.tsx
+++ src/screens/Tasks/TaskTile.tsx
@@ -1,65 +1,59 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TaskTile({ task, onUpdateTask, onDeleteTask }: any) {
-  const onChangeStatus = () => {
-    onUpdateTask(task.id);
-  };
-  const onDelete = () => {
-    onDeleteTask(task.id);
-  };
-  let icon;
-  icon = (
-    <Image
-      style={styles.icon}
-      source={
-        task.isCompleted
-          ? require("../../../assets/icons/icon_check.png")
-          : require("../../../assets/icons/icon_circle.png")
-      }
-    />
-  );
+    const onChangeStatus = () => {
+        onUpdateTask(task.id);
+    };
+    const onDelete = () => {
+        onDeleteTask(task.id);
+    };
+    let icon;
+    icon = task.isCompleted ? (
+        <FontAwesome name="check-square" size={24} color="white" />
+    ) : (
+        <FontAwesome name="square" size={24} color="white" />
+    );
 
-  return (
-    <Pressable onPress={onChangeStatus} style={styles.tile}>
-      <View style={styles.subtile}>
-        {icon}
-        <Text style={styles.text}>{task.title}</Text>
-      </View>
-      <Pressable onPress={onDelete}>
-        <Image
-          style={styles.icon}
-          source={require("../../../assets/icons/icon_bin.png")}
-        />
-      </Pressable>
-    </Pressable>
-  );
+    return (
+        <Pressable onPress={onChangeStatus} style={styles.tile}>
+            <View style={styles.subtile}>
+                {icon}
+                <Text style={styles.text}>{task.title}</Text>
+            </View>
+            <Pressable onPress={onDelete}>
+                <Ionicons name="trash-bin-sharp" size={24} color="white" />
+            </Pressable>
+        </Pressable>
+    );
 }
 
 const styles = StyleSheet.create({
-  tile: {
-    backgroundColor: "#0E6BA8",
-    flex: 1,
-    height: 50,
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginTop: 20,
-    borderRadius: 10,
-    flexDirection: "row",
-    padding: 10,
-  },
-  subtile: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  text: {
-    marginLeft: 20,
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "left",
-  },
-  icon: {
-    width: 20,
-    height: 20,
-  },
+    tile: {
+        backgroundColor: "#0E6BA8",
+        flex: 1,
+        height: 50,
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginTop: 20,
+        borderRadius: 10,
+        flexDirection: "row",
+        padding: 10,
+    },
+    subtile: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    text: {
+        marginLeft: 20,
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "left",
+    },
+    icon: {
+        width: 20,
+        height: 20,
+    },
 });
